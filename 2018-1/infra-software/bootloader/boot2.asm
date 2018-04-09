9abf52bc10e82f26f984d0bb90ad256c8d068f20
Thiago Augusto dos Santos Martins <tasm2@ug2c22.windows.cin.ufpe.br>
2018-04-09 17:31:57 -0300

    added boot2 working files

--- 2018-1/infra-software/bootloader/boot2.asm
+++ 2018-1/infra-software/bootloader/boot2.asm
@@ -1,12 +1,15 @@
 org 0x500; 
 jmp 0x000: start
 
-str1 db 'Loading structures for the kernel',0
+str1 db 'Loading structures for the kernel', 0
 str2 db 'Setting up protected mode', 0
 str3 db 'Loading kernel in memory', 0
 str4 db 'Running kernel', 0
+str5 db 'Wait a minute', 0
+str6 db 'We are setting everything up to you', 0
+str7 db 'Now it is okay', 0
 dot db '.', 0
-finalDot db '.', 10, 13, 0
+dotBreakLine db '.', 10, 13, 0
 
 
 printStrings:
@@ -18,10 +21,47 @@ delay:
 	dec bp
 	nop
     nop
+    nop
+    nop
 	jnz back
 	dec dx
 	cmp dx,0    
 	jnz back
+ret
+
+printString: 
+;; Printa a string que esta em si    
+	
+	lodsb
+	cmp al, 0
+	je exit
+
+	mov ah, 0xe
+    mov bl, 0xf
+	int 10h	
+
+	mov dx, 100;tempo do delay
+	call delay 
+	
+	jmp printString
+exit:
+    ret
+
+print3Dots:
+    mov dx, 10;tempo do delay
+    call delay
+    mov si, dot
+    call printString
+
+    mov dx, 10;tempo do delay
+    call delay
+    mov si, dot
+    call printString
+    
+    mov dx, 10;tempo do delay
+    call delay
+    mov si, dotBreakLine
+    call printString
 ret
 
 start:
@@ -34,7 +74,78 @@ start:
     mov es, ax
     xor bx, bx   ;posição = es<<1+bx
 
+    ; Modo para utilização de vídeo e VGA
+	; mov ah, 0
+	; mov al, 12h
+	; int 10h
+
+    ; Entrando no modo para utilização de vídeo e VGA
+	mov ah, 0
+	mov al, 12h
+	int 10h
+
+    ;Printando Strings
+    mov si, str1
+    call printString
+    call print3Dots
+
+    mov si, str2
+    call printString
+    call print3Dots
+
+    mov si, str3
+    call printString
+    call print3Dots
+
+    mov si, str4
+    call printString
+    call print3Dots
+
+    mov si, str4
+    call printString
+    call print3Dots
+
+    mov si, str4
+    call printString
+    call print3Dots
+
+    mov si, str4
+    call printString
+    call print3Dots
+
+    mov si, str5
+    call printString
+    call print3Dots
+
+    call print3Dots
+    call print3Dots
+
+    mov si, str6
+    call printString
+    call print3Dots
+
+    call print3Dots
+    call print3Dots
+
+    mov si, str7
+    call printString
+    call print3Dots
+    
+;Carrega na memoria o kernel
+	mov ax, 0x7e0
+    mov es, ax
+    xor bx, bx
+
+;Resetando o disco floppy, forçando também a setar todas as trilhas para 0
+reset:
+	mov ah,0		
+	mov dl,0		
+	int 13h			
+	jc reset		;em caso de erro, tenta de novo, 
+
+
 load_kernel:
+
     mov ah, 02h ;lê um setor do disco
     mov al, 20   ;quantidade de setores ocupados pelo boot2
     mov ch, 0   ;track 0
--- 2018-1/infra-software/bootloader/kernel.asm
+++ 2018-1/infra-software/bootloader/kernel.asm
@@ -1,6 +1,51 @@
 org 0x7e00
 jmp 0x0000:start
 
+; story1 db 'Loading structures for the kernel', 0
+; breakLine db '.', 10, 13, 0
+l1q1 db 'a', 0
+l2q1 db 'aaa', 0
+l3q1 db 'aaaaa', 0
+l4q1 db 'aaaaaaa', 0
+l5q1 db 'aaaaaaaaa', 0
+l6q1 db 'aaaaaaaaaaa', 0
+l7q1 db 'aaaaaaaaaaaaa', 0
+jmpLine db 'a', 10, 13, 0
+
+
+delay: 
+;; Função que aplica um delay(improvisado) baseado no valor de dx
+	mov bp, dx
+	back:
+	dec bp
+	nop
+    nop
+    nop
+    nop
+	jnz back
+	dec dx
+	cmp dx,0    
+	jnz back
+ret
+
+printString: 
+;; Printa a string que esta em si    
+	
+	lodsb
+	cmp al, 0
+	je exit
+
+	mov ah, 0xe
+    mov bl, 0xf
+	int 10h	
+
+	mov dx, 100;tempo do delay
+	call delay 
+	
+	jmp printString
+exit:
+    ret
+
 start:
 	xor ax, ax
 	mov ds, ax
@@ -11,6 +56,40 @@ start:
 	mov al, 12h
 	int 10h
 
+	mov si, l1q1
+	call printString
+	mov si, jmpLine
+	call printString
+
+	mov si, l2q1
+	call printString
+	mov si, jmpLine
+	call printString
+
+	mov si, l3q1
+	call printString
+	mov si, jmpLine
+	call printString
+	
+	mov si, l4q1
+	call printString
+	mov si, jmpLine
+	call printString
+
+	mov si, l5q1
+	call printString
+	mov si, jmpLine
+	call printString
+
+	mov si, l6q1
+	call printString
+	mov si, jmpLine
+	call printString
+
+	mov si, l7q1
+	call printString
+	mov si, jmpLine
+	call printString
 
 done:
 	jmp $
